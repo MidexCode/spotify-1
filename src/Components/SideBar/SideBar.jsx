79171eee0ba60bb01e59b819eec95ba07cab267e
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2024-11-20 22:59:39 -0800

    Done with the side after i will add the player section

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -5,10 +5,43 @@ export const SideBar = () => {
   return (
     <>
       <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
-        <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
+        <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around ">
           <div className="flex items-center gap-3 pl-8 cursor-pointer">
-            <img src={assets.home_icon} alt="" />
+            <img src={assets.home_icon} alt="" className="w-6" />
             <p className="font-bold">Home</p>
+          </div>
+
+          <div className="flex items-center gap-3 pl-8 cursor-pointer">
+            <img src={assets.search_icon} alt="" className="w-6" />
+            <p className="font-bold">Search</p>
+          </div>
+        </div>
+
+        <div className="bg-[#121212] h-[85%] rounded">
+          <div className="p-4 flex items-center justify-between ">
+            <div className="flex items-center gap-3 cursor-pointer">
+              <img src={assets.stack_icon} alt="" className="w-8" />
+              <p className="font-semibold">Your Library</p>
+            </div>
+
+            <div className="flex items-center gap-3 cursor-pointer">
+              <img src={assets.arrow_icon} alt="" className="w-5" />
+              <img src={assets.plus_icon} alt="" className="w-5" />
+            </div>
+          </div>
+
+          <div className="p-4 bg-[#242424] flex  justify-start gap-24 items-center m-2 mt-4 rounded font-semibold pl-4">
+            <h1>Create Your First Playlist</h1>
+            <button className="px-4 py-1.5 bg-white  text-[15px] text-black rounded-full ">
+              Create Playlist
+            </button>
+          </div>
+
+          <div className="p-4 bg-[#242424] flex  justify-start gap-8 items-center m-2 mt-4 rounded font-semibold pl-4">
+            <h1>Add your Favourite Podcast you listen to</h1>
+            <button className="px-4 py-1.5 bg-white  text-[15px] text-black rounded-full ">
+              Add Podcast
+            </button>
           </div>
         </div>
       </div>
