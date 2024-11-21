c7f2fb477f722e344424731ed2e4e3da451b28f6
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2024-11-21 06:08:09 -0800

    done with the navbar now working on the albumitems

---
+++ src/Components/AlbumItem/AlbumItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const AlbumItem = ({ image, name, desc, id }) => {
+  return (
+    <div className="min-w-[180px] cursor-pointer p-2 px-3 rounded hover:bg-[#FFFFff26]">
+      <div></div>
+    </div>
+  );
+};
--- src/Components/Display/DisplayHome.jsx
+++ src/Components/Display/DisplayHome.jsx
@@ -5,6 +5,8 @@ export const DisplayHome = () => {
   return (
     <>
       <Navbar />
+
+      <div className="my-5 font-bold text-2xl"></div>
     </>
   );
 };
