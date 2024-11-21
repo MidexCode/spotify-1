faf6e50570576ffdf7af1e251c5104748b44c254
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2024-11-21 06:25:03 -0800

    Still working on the AlbumItem section

--- src/Components/AlbumItem/AlbumItem.jsx
+++ src/Components/AlbumItem/AlbumItem.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export const AlbumItem = ({ image, name, desc, id }) => {
   return (
     <div className="min-w-[180px] cursor-pointer p-2 px-3 rounded hover:bg-[#FFFFff26]">
-      <div></div>
+      <img src={name} alt="" className="rounded" />
+      <p className="font-bold mt-2 mb-1">{name}</p>
     </div>
   );
 };
