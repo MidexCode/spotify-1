9bac30313bb23649be3301cfd328cc27773e56f6
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2024-11-22 22:13:40 -0800

    Working on the display home and done with the AlbumItem

--- src/Components/AlbumItem/AlbumItem.jsx
+++ src/Components/AlbumItem/AlbumItem.jsx
@@ -3,8 +3,9 @@ import React from "react";
 export const AlbumItem = ({ image, name, desc, id }) => {
   return (
     <div className="min-w-[180px] cursor-pointer p-2 px-3 rounded hover:bg-[#FFFFff26]">
-      <img src={name} alt="" className="rounded" />
-      <p className="font-bold mt-2 mb-1">{name}</p>
+      <img src={image} alt="" className="rounded" />
+      <p className="font-bold mt-2 mb-1 text-slate-200">{name}</p>
+      <p className="text-slate-200 text-sm">{desc}</p>
     </div>
   );
 };
--- src/Components/Display/DisplayHome.jsx
+++ src/Components/Display/DisplayHome.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Navbar } from "../Navbar/Navbar";
+import { albumsData } from "../../assets/assets";
+import { AlbumItem } from "../AlbumItem/AlbumItem";
 
 export const DisplayHome = () => {
   return (
     <>
       <Navbar />
 
-      <div className="my-5 font-bold text-2xl"></div>
+      <div className="my-5 font-bold text-2xl">
+        {albumsData.map((item, index) => (
+          <AlbumItem
+            key={index}
+            name={item.name}
+            desc={item.desc}
+            id={item.id}
+            image={item.image}
+          />
+        ))}
+      </div>
     </>
   );
 };
